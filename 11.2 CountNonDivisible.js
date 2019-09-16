c929e0659f73b302c50d32d2567ff758744e86ff
Eduardo Brasil <brasilpeixoto@hotmail.com>
2019-09-16 10:05:34 -0300

    Solving

--- 11.2 CountNonDivisible.js
+++ 11.2 CountNonDivisible.js
@@ -1,12 +1,36 @@
 function solution(A) {
-    let B = A.map(x => x);
-    B.sort((a, b) => { return a - b });
-    let max = B[B.length - 1];
-    let vet = new Array(max + 1).fill(false);
-    for(let i = 0; i < A.length; i++){
-        f
+    let l = A.length;
+    let sorted = A.map(x => x).sort((a, b) => { return a - b });
+    let max = sorted[l - 1];
+
+    let div = new Array(max + 1).fill(false);
+    
+    for (let i = 0; i < l; i++) {
+        if (!div[A[i]]) {
+            div[A[i]] = 1;
+        }
+        else {
+            div[A[i]]++;
+        }
     }
+    
+    for (let i = 0; i < l; i++) {
+        for(let k = 2; A[i] * k <= max; k++){
+            if(div[A[i] * k]){
+                div[A[i] * k]++;
+            }
+        }
+    }
+
+    let result = [];
+    for (let i = 0; i < l; i++) {
+        result[i] = A.length - div[A[i]];
+    }
+
+    return result;
 }
 
 solution([3, 1, 2, 3, 6]);
 // expected [2, 4, 3, 2, 0]
+
+// https://app.codility.com/demo/results/trainingH23DD8-2SX/
