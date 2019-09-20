6c7f4edcbe2311c416fe6057ba93d0ae7622c7b5
Eduardo Brasil <brasilpeixoto@hotmail.com>
2019-09-20 14:06:04 -0300

    Solving

--- 15.1 CountDistinctSlices.js
+++ 15.1 CountDistinctSlices.js
@@ -1,10 +1,9 @@
 function solution(M, A) {
-    let set = new Set();
-    let n = 0;
-    for (let i = 0; i < A.length; i++) {
-        set.add(A[i]);
-        if(set.size != n){
-            n = set.size
-        }
-    }
+
 }
+
+solution(7, [1, 2, 3, 4, 5, 3, 6, 7]);
+// expected 27
+
+solution(6, [3, 4, 5, 5, 2]);
+// expected 9
