dc063e00e4642c3c56a247e3f21c04ac0b2b411e
Eduardo Brasil <brasilpeixoto@hotmail.com>
2019-09-26 10:06:53 -0300

    Solving

--- 15.2 CountTriangles.js
+++ 15.2 CountTriangles.js
@@ -1,21 +1,17 @@
 function solution(A) {
     let result = 0, q, r;
     A.sort((a, b) => { return a - b });
-    debugger;
-    for (let p = 0; p < A.length -2; p++) {
+    for (let p = 0; p < A.length - 2; p++) {
         q = p + 1;
         r = p + 2;
 
-        while (r < A.length) {
-            if (A[p] + A[q] > A[r]) {
-                result++;
+        while (q < A.length - 1) {
+            if (r < A.length && A[p] + A[q] > A[r]) {
                 r++;
             }
             else {
+                result += r - q - 1;
                 q++;
-                if (q == r) {
-                    r = q + 1;
-                }
             }
         }
     }
@@ -24,3 +20,5 @@ function solution(A) {
 
 solution([10, 2, 5, 1, 8, 12]);
 // expected 4
+
+// https://app.codility.com/demo/results/trainingBF9TVD-PHF/
